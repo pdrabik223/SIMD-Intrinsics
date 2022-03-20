ec934eee53d9f3c707d66566d9da57179fafb47f
piotr233 <pdrabik223@gmail.com>
2022-03-20 13:15:41 +0100

    New hard.cxx file

---
+++ hard.cxx
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <immintrin.h>
+#include <tmmintrin.h>
+#include <smmintrin.h>
+#include <immintrin.h>
+#include <vector>
+#include <chrono>
+#include <functional>
+#include <cassert>
+
+int main(){
+
+  std::cout<<"correct cmake";
+
+  return 0;
+}
