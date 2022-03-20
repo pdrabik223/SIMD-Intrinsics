efd345afa3c056a5eff0f70487b866b39b0d1469
piotr233 <pdrabik223@gmail.com>
2022-03-20 13:16:05 +0100

    Simple yet ineffective example

--- main.cxx
+++ main.cxx
@@ -7,6 +7,7 @@
 #include <immintrin.h>
 #include <tmmintrin.h>
 #include <smmintrin.h>
+#include <immintrin.h>
 #include <vector>
 
 template<class T>
@@ -17,8 +18,9 @@ double Avg(const std::vector<T> &vector) {
 }
 #include <chrono>
 #include <functional>
+#include <cassert>
 
-#define ARRAY_SIZE (1024*1024*300)
+#define ARRAY_SIZE (1024*1024)
 
 using std::chrono::high_resolution_clock;
 using std::chrono::microseconds;
@@ -32,17 +34,17 @@ std::cout << name << " took:\t" << std::chrono::duration_cast<std::chrono::micro
 std::vector<int32_t> array(ARRAY_SIZE, 0);
 std::vector<int32_t> array2(ARRAY_SIZE, 0);
 
-std::vector<microseconds> measure_time(std::function<void(void)> function, size_t samples = 1000) {
-  std::vector<microseconds> results;
+std::vector<size_t> measure_time(std::function<void(void)> function, size_t samples = 1000) {
+  std::vector<size_t> results;
   results.reserve(samples);
 
   for (size_t i = 0; i < samples; i++) {
 //    std::fill(array.begin(), array.end(), 0);
-//    std::fill(array.begin(), array.end(), 0);
-    auto start = high_resolution_clock::now();
-    function();
-    auto end = high_resolution_clock::now();
-    results.push_back(std::chrono::duration_cast<microseconds>(end - start));
+//    std::fill(array2.begin(), array2.end(), 0);
+	auto start = high_resolution_clock::now();
+	function();
+	auto end = high_resolution_clock::now();
+	results.push_back(std::chrono::duration_cast<microseconds>(end - start).count());
   }
   return results;
 }
@@ -50,31 +52,43 @@ std::vector<microseconds> measure_time(std::function<void(void)> function, size_
 int main() {
   srand(time(NULL));
   std::function<void(void)> using_simd = []() {
-    for (size_t i = 0; i < ARRAY_SIZE / 4; i += 4) {
-      __m128i packed = _mm_set_epi32(array[i], array[i + 1], array[i + 2], array[i + 3]);
-      __m128i indexes = _mm_set_epi32(i, i + 1, i + 2, i + 3);
-      __m128i result = _mm_add_epi32(packed, indexes);
-      array[i] = _mm_extract_epi32(result, 3);
-      array[i + 1] = _mm_extract_epi32(result, 2);
-      array[i + 2] = _mm_extract_epi32(result, 1);
-      array[i + 3] = _mm_extract_epi32(result, 0);
-    }
+	__m128i packed;
+	__m128i indexes;
+	__m128i result;
+
+	for (size_t i = 0; i < ARRAY_SIZE; i += 4) {
+	  packed = _mm_set_epi32(array[i], array[i + 1], array[i + 2], array[i + 3]);
+	  indexes = _mm_set_epi32(i, i + 1, i + 2, i + 3);
+	  result = _mm_add_epi32(packed, indexes);
+
+	  array[i] = _mm_extract_epi32(result, 3);
+	  array[i + 1] = _mm_extract_epi32(result, 2);
+	  array[i + 2] = _mm_extract_epi32(result, 1);
+	  array[i + 3] = _mm_extract_epi32(result, 0);
+	}
   };
+
   std::function<void(void)> using_simple_loop = []() {
-    for (size_t i = 0; i < ARRAY_SIZE; i++) {
-      array2[i] += i;
-    }
+	for (size_t i = 0; i < ARRAY_SIZE; i++) {
+	  array2[i] += i ;
+	}
   };
 
-  auto simd_times = measure_time(using_simd);
-  auto loop_times = measure_time(using_simple_loop);
+  std::cout << "SIMD...\t";
+  auto simd_times = measure_time(using_simd, 1000);
+  std::cout << "done\nSimple loop...\t";
+  auto loop_times = measure_time(using_simple_loop, 1000);
 
-  std::cout << "Validation ... ";
-  for (int i = 0; i < ARRAY_SIZE; i++) {
-    if (array[i] != array2[i]) {
-      throw std::runtime_error("Values in both arrays are not the same");
-    }
+  std::cout << "done\nValidation ... ";
+  for (int i = 0; i < ARRAY_SIZE-1; i++) {
+	if (array[i] != array2[i]) {
+	  assert(false);
+	  throw std::runtime_error("Values in both arrays are not the same");
+	}
   }
-  std::cout << "ok";
+  std::cout << "ok\n";
+
+  std::cout << "Average time Using SIMD:\t" << Avg(simd_times) << " microseconds\n";
+  std::cout << "Average time Using Simple Loop:\t" << Avg(loop_times) << " microseconds\n";
   return 0;
 }
